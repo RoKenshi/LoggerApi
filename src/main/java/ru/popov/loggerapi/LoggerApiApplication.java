2ef96055b55dd49e590e7d07364cab22eb998037
dondigidon <rouronikenshi@gmail.com>
2023-11-08 16:41:10 +0300

    start app

---
+++ src/main/java/ru/popov/loggerapi/LoggerApiApplication.java
@@ -0,0 +1,13 @@
+package ru.popov.loggerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerApiApplication.class, args);
+	}
+
+}
